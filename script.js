6c996a713fd9eb7d1609f9fd3a3943c9b60ed31e
Florian Louaisil <florian.louaisil02@gmail.com>
2025-02-13 23:14:45 +0100

    Final

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Récupérer tous les boutons qui ouvrent des modales
+    const openModalButtons = document.querySelectorAll('.openModalBtn');
+
+    // Récupérer toutes les modales
+    const modals = document.querySelectorAll('.modal');
+
+    // Fonction pour ouvrir la modale
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const modalId = button.getAttribute('data-modal');
+            const modal = document.getElementById(modalId);
+            if (modal) {
+                modal.style.display = 'block';
+            }
+        });
+    });
+
+    // Fonction pour fermer la modale
+    modals.forEach(modal => {
+        const closeButton = modal.querySelector('.close-btn');
+        closeButton.addEventListener('click', function () {
+            modal.style.display = 'none';
+        });
+    });
+
+    // Fermer la modale si l'utilisateur clique en dehors de la fenêtre modale
+    window.addEventListener('click', function (e) {
+        modals.forEach(modal => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
+    });
+});
