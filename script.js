4ac00e971516fcd89f628bfd0833a5449e54163d
Florian Louaisil <florian.louaisil02@gmail.com>
2025-02-17 11:50:01 +0100

    Update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const modalId = button.getAttribute('data-modal');
             const modal = document.getElementById(modalId);
             if (modal) {
-                modal.style.display = 'flex';
+                modal.style.display = 'block';
             }
         });
     });
