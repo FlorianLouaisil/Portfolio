c66b49bd05a9986f95838081d68e0d091f92eb5a
Florian Louaisil <florian.louaisil02@gmail.com>
2025-02-17 13:10:51 +0100

    Update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Récupérer tous les boutons qui ouvrent des modales
+
     const openModalButtons = document.querySelectorAll('.openModalBtn');
 
-    // Récupérer toutes les modales
+
     const modals = document.querySelectorAll('.modal');
 
     // Fonction pour ouvrir la modale
