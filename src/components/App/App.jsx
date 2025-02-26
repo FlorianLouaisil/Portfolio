77faa9541feda4271eeb3d75b8e1ab4290808d92
Florian Louaisil <florian.louaisil02@gmail.com>
2025-02-26 13:26:05 +0100

    Update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,32 +14,32 @@ function App() {
           <a href="#contact">Contact</a>
         </nav>
       </header>
-
-      <section id="about">
-        <div className="description">
-          <h2>Ma description</h2>
-          <p>Je suis Louaisil Florian, 22 ans, en reconversion professionnelle vers le développement web.
-            Passionné par la création et toujours curieux d'apprendre, je me consacre à la réalisation de
-            projets qui allient innovation et fonctionnalité. Mon objectif est de progresser constamment et de
-            contribuer à des solutions numériques impactantes.</p>
-          <a href="CV_Florian_Louaisil.pdf" className="download-button">Télécharger mon CV</a>
-        </div>
-      </section>
-
       <main>
+        <section id="about">
+          <div className="description">
+            <h2>Ma description</h2>
+            <p>Je suis Louaisil Florian, 22 ans, en reconversion professionnelle vers le développement web.
+              Passionné par la création et toujours curieux d'apprendre, je me consacre à la réalisation de
+              projets qui allient innovation et fonctionnalité. Mon objectif est de progresser constamment et de
+              contribuer à des solutions numériques impactantes.</p>
+            <a href="CV_Florian_Louaisil.pdf" className="download-button">Télécharger mon CV</a>
+          </div>
+        </section>
+
+
         <Project />
         <Skills />
+
+
+        <section id="contact">
+          <h2>Contact</h2>
+          <p>Voici mes coordonnées pour me contacter :</p>
+          <ul>
+            <li><strong>Email :</strong> <a href="mailto:florian.louaisil02@gmail.com">florian.louaisil02@gmail.com</a>
+            </li>
+          </ul>
+        </section>
       </main>
-
-      <section id="contact">
-        <h2>Contact</h2>
-        <p>Voici mes coordonnées pour me contacter :</p>
-        <ul>
-          <li><strong>Email :</strong> <a href="mailto:florian.louaisil02@gmail.com">florian.louaisil02@gmail.com</a>
-          </li>
-        </ul>
-      </section>
-
       <footer>
         <a href="https://www.linkedin.com/in/florian-louaisil-533395238" target="_blank">LinkedIn</a>
         <a href="https://github.com/FlorianLouaisil" target="_blank">GitHub</a>
